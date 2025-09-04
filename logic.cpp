f7934db1de6ff0e7c5e410d46cd5f3b819b472cd
Avani Mandlik <avanimandlik27@gmail.com>
2025-09-04 17:39:30 +0530

    Update Wordle logic to handle feedback

--- logic.cpp
+++ logic.cpp
@@ -1,1 +1,37 @@
-"//Wordle logic goes here" 
+//Wordle logic goes here
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string word;
+    cin>>word;
+    int count;
+    string guess = "";
+    while(count!=0){
+        cin>>guess;
+        bool used[5] = {false,false,false,false,false};
+        count++;
+        string output(5, '-');
+        for(int i=0; i<5; i++){
+            if(guess[i]==word[i]){
+                output[i]='G';
+                used[i] = true;
+            }
+            else{
+                for(int j=0; j<5; j++){
+                    if(guess[i]==word[j] && !used[i]){
+                        output[i]='Y';
+                        used[j]=true;
+                    }
+                }
+            }
+        }
+        if(output == "GGGGG"){
+            cout<<"You Win!"<<endl;
+            break;
+        }
+        cout<<output<<endl;
+    }
+}
